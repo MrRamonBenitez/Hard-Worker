5b47df450475561d3b3288d1ed534eeb7af83676
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2021-12-25 18:28:24 +0500

    .gitignore added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errMsg = System.out::println;
+
+        Worker worker = new Worker(listener, errMsg);
+        worker.start();
+
+    }
+}
